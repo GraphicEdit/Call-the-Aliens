3cb22e9799e0b5751673f30cb29bc4b9377f4eeb
Bartosz Mazur <bartosz.mazur90@gmail.com>
2019-01-22 16:44:45 +0100

    signal bouncing prototype

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private GameObject dishMount;
+    [SerializeField] private GameObject signalSpawn;
+    [SerializeField] private GameObject signalPrefab;
+
+    [SerializeField] private float signalForce;
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        HandleDishRotation();
+
+        if (Input.GetMouseButtonDown(0)) {
+            Shoot();
+        }
+    }
+
+    void HandleDishRotation() {
+        Vector3 newDishDirection = (Vector2) Camera.main.ScreenToWorldPoint(Input.mousePosition) - (Vector2) transform.position;
+        newDishDirection = newDishDirection.normalized;
+        dishMount.transform.up = newDishDirection;
+    }
+
+    void Shoot() {
+        Vector2 direction = dishMount.transform.up;
+        GameObject newSignal = Instantiate(signalPrefab);
+        newSignal.transform.position = signalSpawn.transform.position;
+        newSignal.GetComponent<Rigidbody2D>().AddForce(direction * signalForce, ForceMode2D.Impulse);
+    }
+}
